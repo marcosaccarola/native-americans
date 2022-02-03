bd3022ae4aa948b0af753dbc8eed5978c6b31eec
marcosaccarola <marco.sacca.ads@gmail.com>
2022-02-03 16:43:42 +0100

    Support

--- src/components/02-MainEvent.jsx
+++ src/components/02-MainEvent.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Image, Row } from "react-bootstrap"
+import { Button, Col, Container, Image, Row } from "react-bootstrap"
 import {ImStarFull} from 'react-icons/im'
 import photo0 from '../assets/mainEvent0.jpg'
 import photo1 from '../assets/mainEvent1.jpg'
@@ -12,25 +12,26 @@ const MainEvent=()=>{
         <Container className=''>
             <Row className='pt-5'>
                 <Col className='display-6 text-start'>
-                    <ImStarFull className='me-3' />
+                    <ImStarFull className='me-4' />
                     Questa è la prossima attività:
                 </Col>
             </Row>           
             <Row className='pt-5'>
-                <Col md={8} className='justify'>
+                <Col md={8} className='justify pb-3'>
                     <p>
                     Il modo migliore per spiegare all'utente chi siamo è farlo attraverso delle attività.
-                     Qui indicheremo l'evento principale dell'anno.
-                     Se non abbiamo nessun avento in programma allora qui indicheremo un progetto,
-                     una cosa che facciamo insieme e che è il motivo principale per il quale l'associazione esiste.
+                    Qui indicheremo l'evento principale dell'anno.
+                    Se non abbiamo nessun evento in programma allora qui indicheremo un progetto,
+                    una cosa che facciamo insieme e che è il motivo principale per il quale l'associazione esiste.
                     </p>
                     <p>
                     A destra ci mettiamo anche due foto, che però nella versione per telefono cellulare le ho messe subito sotto a questo testo.
                     </p>
                     <p>
-                    Metto un bottone che invita l'utente a fare qualcosa, perché è importante che gli utenti facciano cose,
-                     quando entrano in un sito tipo il nostro.
+                    Metto anche un bottone che invita l'utente a fare qualcosa, perché è importante che l'utente faccia cose,
+                    quando entra nel nostro sito.
                     </p>
+                    <Button variant="dark my-3">Informazioni</Button>
                 </Col>
                 <Col md={4}>
                     <Image src={photo0} className='img-fluid rounded mb-3' />
--- src/components/03-Support.jsx
+++ src/components/03-Support.jsx
@@ -1,11 +1,42 @@
-
+import { Button, Col, Container, Image, Row } from "react-bootstrap"
+import {RiHandCoinLine} from 'react-icons/ri'
+import photo0 from '../assets/mainEvent0.jpg'
 
 
 const Support=()=>{
 
     return(
         <>
-        SUPPORT
+        <Container fluid className='mt-5 bg-danger text-white'>
+        <Container>
+            <Row className='pt-5'>
+                    <Col className='display-6 text-start'>
+                        <RiHandCoinLine className='me-4' />
+                        Abbiamo bisogno del tuo supporto.
+                    </Col>
+                </Row>           
+                <Row className='pt-5'>
+                    <Col md={4} md={{order:'first'}} xs={{order:'last'}}>
+                        <Image src={photo0} className='img-fluid rounded mb-3' />
+                    </Col>
+                    <Col md={8} className='justify pb-3'>
+                        <p>
+                        Come hai notato, qui lo sfondo diventa rosso perché questa è una sezione importante.
+                        </p>
+                        <p>
+                        La prima cosa che vogliamo l'abbiamo scritta sopra: registrati a questo sito e segui le nostre attività.
+                        La seconda cosa a cui teniamo sono le donazioni, perché con più donazioni potremmo svolgere attività migliori e rivolte a un pubblico più vasto.
+                        </p>
+                        <p>
+                        A sinistra c'è una foto perché questo semplifica la lettura sugli schermi molto grandi.
+                        Nella versione per cellulare, la foto appare sempre dopo il testo.
+                        Anche in questo caso mettiamo un bel bottone, perché ci piacciono le cose semplici.
+                        </p>
+                        <Button variant="success my-3">Sostienici</Button>
+                    </Col>
+                </Row>  
+        </Container>
+        </Container>
         </>
     )
 }
