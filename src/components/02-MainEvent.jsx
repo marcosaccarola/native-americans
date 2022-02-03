0d9949d80d8b91198a45509233581908931b6cc1
marcosaccarola <marco.sacca.ads@gmail.com>
2022-02-03 16:11:16 +0100

    Intro

--- src/components/02-MainEvent.jsx
+++ src/components/02-MainEvent.jsx
@@ -11,14 +11,26 @@ const MainEvent=()=>{
         <>
         <Container className=''>
             <Row className='pt-5'>
-                <Col><ImStarFull className='me-3' />Questa è la prossima attività dell'associazione:</Col>
+                <Col className='display-6 text-start'>
+                    <ImStarFull className='me-3' />
+                    Questa è la prossima attività:
+                </Col>
             </Row>           
-            <Row className='pt-5 px-3'>
+            <Row className='pt-5'>
                 <Col md={8} className='justify'>
+                    <p>
                     Il modo migliore per spiegare all'utente chi siamo è farlo attraverso delle attività.
                      Qui indicheremo l'evento principale dell'anno.
                      Se non abbiamo nessun avento in programma allora qui indicheremo un progetto,
                      una cosa che facciamo insieme e che è il motivo principale per il quale l'associazione esiste.
+                    </p>
+                    <p>
+                    A destra ci mettiamo anche due foto, che però nella versione per telefono cellulare le ho messe subito sotto a questo testo.
+                    </p>
+                    <p>
+                    Metto un bottone che invita l'utente a fare qualcosa, perché è importante che gli utenti facciano cose,
+                     quando entrano in un sito tipo il nostro.
+                    </p>
                 </Col>
                 <Col md={4}>
                     <Image src={photo0} className='img-fluid rounded mb-3' />
