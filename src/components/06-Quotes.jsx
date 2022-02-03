3fe48d44e6c24e6d4b019490263c8105f9b5cc45
marcosaccarola <marco.sacca.ads@gmail.com>
2022-02-03 17:59:00 +0100

    Blockquote

--- src/components/06-Quotes.jsx
+++ src/components/06-Quotes.jsx
@@ -1,11 +1,20 @@
-
+import { Container } from "react-bootstrap"
 
 
 const Quotes=()=>{
 
     return(
         <>
-        QUOTES
+        <Container className='mt-5 text-start'>
+            <figure>
+            <blockquote class="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+            </blockquote>
+            <figcaption class="blockquote-footer">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+            </figcaption>
+            </figure>
+        </Container>
         </>
     )
 }
