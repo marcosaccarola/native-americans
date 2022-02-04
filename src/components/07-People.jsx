5f3c8cb8c79402dfe6eb76269067a94d2f662192
marcosaccarola <marco.sacca.ads@gmail.com>
2022-02-04 01:09:55 +0100

    Staff

--- src/components/07-People.jsx
+++ src/components/07-People.jsx
@@ -1,3 +1,8 @@
+import { Col, Container, Image, Row } from "react-bootstrap"
+import {ImProfile} from 'react-icons/im'
+import presidente from '../assets/presidente.jpg'
+import vicepresidente from '../assets/vice.jpg'
+import segretario from '../assets/segretario.jpg'
 
 
 
@@ -5,7 +10,31 @@ const People=()=>{
 
     return(
         <>
-        PEOPLE
+        <Container>
+            <Row className='pt-5'>
+                <Col className='display-6 text-start'>
+                    <ImProfile className='me-4' />
+                    Chi siamo
+                </Col>
+            </Row> 
+            <Row className='pt-5'>
+                <Col md={4} className=''>
+                    <Image src={presidente} width={200} className='rounded-circle img-fluid'/>
+                    <h6>Presidente</h6>
+                    <p className='text-muted'>Nome e cognome</p>
+                </Col>
+                <Col md={4} className=''>
+                    <Image src={vicepresidente} width={200} className='rounded-circle img-fluid'/>
+                    <h6>Vicepresidente</h6>
+                    <p className='text-muted'>Nome e cognome</p>
+                </Col>
+                <Col md={4} className=''>
+                    <Image src={segretario} width={200} className='rounded-circle img-fluid'/>
+                    <h6>Segretario</h6>
+                    <p className='text-muted'>Nome e cognome</p>
+                </Col>
+            </Row>
+        </Container>
         </>
     )
 }
