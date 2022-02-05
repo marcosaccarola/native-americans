85dca493a1f798dca8c715b538d9a14aa009918e
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-02-05 22:31:12 +0100

    Sign in/up

--- src/components/08-RegisterAgain.jsx
+++ src/components/08-RegisterAgain.jsx
@@ -1,31 +1,31 @@
 import { Button, Carousel, Col, Container, Image, Placeholder, Row } from "react-bootstrap"
-import {BsFillShieldLockFill} from 'react-icons/bs'
+import { BsFillShieldLockFill } from 'react-icons/bs'
 
 
-const RegisterAgain=()=>{
+const RegisterAgain = () => {
 
-    return(
+    return (
         <>
-        <Container fluid className='mt-5 pb-5 bg-success text-white'>
-            <Container>
-            <Row className='py-5'>
-                <Col className='display-6 text-start'>
-                    <BsFillShieldLockFill className='me-4' />
-                    Accedi al sito
-                </Col>
-            </Row>
-            <Row className='pt-3'>
-                <Col md={12} className='justify pb-3 w-50 mx-auto'>
-                    <p className='mb-4'>Accedi con Google, è facile!</p>
-                    <p>La sicurezza dei tuoi dati sarà garantita da Google e non dovrai ricordare nessuna password!</p>
-                    <Button variant="black border border-white text-white w-100">Accedi con Google</Button>
-                    <p className='mt-5 mb-4'>Iscriviti con email.</p>
-                    <p>Puoi iscriverti indicando la tua email e una password valida.</p>
-                    <Button variant="black border border-white text-white w-100">Iscriviti con Email</Button>
-                </Col>
-            </Row>     
+            <Container fluid className='mt-5 pb-5 bg-register text-white'>
+                <Container>
+                    <Row className='py-5'>
+                        <Col className='display-6 text-start'>
+                            <BsFillShieldLockFill className='me-4' />
+                            Accedi al nostro sito
+                        </Col>
+                    </Row>
+                    <Row className='pt-3'>
+                        <Col md={12} className='justify pb-3 w-50 mx-auto'>
+                            <p className='mb-4'>Accedi con Google, è facile!</p>
+                            <p>La sicurezza dei tuoi dati sarà garantita da Google e non dovrai ricordare nessuna password!</p>
+                            <Button className="bg-black border border-white text-white w-100">Accedi con Google</Button>
+                            <p className='mt-5 mb-4'>Accedi con la tua email.</p>
+                            <p>Puoi accedere indicando la tua email e una password valida.</p>
+                            <Button className="bg-black border border-white text-white w-100">Accedi con Email</Button>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
-        </Container>
         </>
     )
 }
